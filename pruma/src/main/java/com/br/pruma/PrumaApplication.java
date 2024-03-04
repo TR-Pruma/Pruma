142fe640a8b743b7ee4e9c8ec99a80ca7b7318a4
Renato <reviturino@gmail.com>
2024-03-04 15:30:29 -0300

    estrutura do projeto backend

---
+++ pruma/src/main/java/com/br/pruma/PrumaApplication.java
@@ -0,0 +1,13 @@
+package com.br.pruma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrumaApplication.class, args);
+	}
+
+}
---
+++ pruma/src/test/java/com/br/pruma/PrumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pruma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
