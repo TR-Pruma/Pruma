fb78611feceb4906463b789a95a2cbc70888bb50
Renato Viturino <reviturino@gmail.com>
2026-04-04 20:44:30 -0300

    fix(service): corrige tipo do ID Integer->UUID e adiciona AuditoriaUpdateDTO na interface AuditoriaService

--- pruma/src/main/java/com/br/pruma/application/service/AuditoriaService.java
+++ pruma/src/main/java/com/br/pruma/application/service/AuditoriaService.java
@@ -7,12 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface AuditoriaService {
     AuditoriaResponseDTO create(AuditoriaRequestDTO dto);
-    AuditoriaResponseDTO getById(Integer id);
+    AuditoriaResponseDTO getById(UUID id);
     List<AuditoriaResponseDTO> listAll();
     Page<AuditoriaResponseDTO> list(Pageable pageable);
-    AuditoriaResponseDTO update(Integer id, AuditoriaUpdateDTO dto);
-    void delete(Integer id);
+    AuditoriaResponseDTO update(UUID id, AuditoriaUpdateDTO dto);
+    void delete(UUID id);
 }
