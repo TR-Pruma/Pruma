1e73f9eff4b684101f3a9fe99515b9e4e06faca8
Renato Viturino <reviturino@gmail.com>
2026-04-04 12:18:45 -0300

    refactor(structure): move excecao de dominio para core/exception e handler/ApiError para adapters/in/rest/exception

---
+++ pruma/src/main/java/com/br/pruma/core/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.br.pruma.core.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
