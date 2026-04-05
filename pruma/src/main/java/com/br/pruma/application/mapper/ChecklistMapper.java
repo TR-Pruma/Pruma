bb09bd3de757a871d6bc43ff6a673cf0c79f8dec
Renato Viturino <reviturino@gmail.com>
2026-04-04 21:00:19 -0300

    fix(mapper): renomeia toResponseDTO->toResponse e updateEntity->updateFromDto no ChecklistMapper

--- pruma/src/main/java/com/br/pruma/application/mapper/ChecklistMapper.java
+++ pruma/src/main/java/com/br/pruma/application/mapper/ChecklistMapper.java
@@ -2,6 +2,7 @@ package com.br.pruma.application.mapper;
 
 import com.br.pruma.application.dto.request.ChecklistRequestDTO;
 import com.br.pruma.application.dto.response.ChecklistResponseDTO;
+import com.br.pruma.application.dto.update.ChecklistUpdateDTO;
 import com.br.pruma.core.domain.Checklist;
 import com.br.pruma.core.domain.Projeto;
 import org.mapstruct.*;
@@ -9,26 +10,26 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring", uses = {ItemChecklistMapper.class})
 public interface ChecklistMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "nome", source = "nome")
-    @Mapping(target = "projeto", source = "projetoId", qualifiedByName = "projetoIdToProjeto")
-    @Mapping(target = "itens", ignore = true)
-    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "id",              ignore = true)
+    @Mapping(target = "nome",            source = "nome")
+    @Mapping(target = "projeto",         source = "projetoId", qualifiedByName = "projetoIdToProjeto")
+    @Mapping(target = "itens",           ignore = true)
+    @Mapping(target = "dataCriacao",     ignore = true)
     @Mapping(target = "dataAtualizacao", ignore = true)
-    @Mapping(target = "version", ignore = true)
-    @Mapping(target = "ativo", constant = "true")
+    @Mapping(target = "version",         ignore = true)
+    @Mapping(target = "ativo",           constant = "true")
     Checklist toEntity(ChecklistRequestDTO dto);
 
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "nome", source = "nome")
-    @Mapping(target = "projetoId", source = "projeto.id")
-    @Mapping(target = "projetoNome", source = "projeto.nome")
-    @Mapping(target = "itens", source = "itens")
-    @Mapping(target = "ativo", source = "ativo")
-    @Mapping(target = "dataCriacao", source = "dataCriacao")
-    @Mapping(target = "dataAtualizacao", source = "dataAtualizacao")
+    @Mapping(target = "id",                  source = "id")
+    @Mapping(target = "nome",                source = "nome")
+    @Mapping(target = "projetoId",           source = "projeto.id")
+    @Mapping(target = "projetoNome",         source = "projeto.nome")
+    @Mapping(target = "itens",               source = "itens")
+    @Mapping(target = "ativo",               source = "ativo")
+    @Mapping(target = "dataCriacao",         source = "dataCriacao")
+    @Mapping(target = "dataAtualizacao",     source = "dataAtualizacao")
     @Mapping(target = "percentualConcluido", expression = "java(checklist.getPercentualConcluido())")
-    ChecklistResponseDTO toResponseDTO(Checklist checklist);
+    ChecklistResponseDTO toResponse(Checklist checklist);
 
     @Named("projetoIdToProjeto")
     default Projeto projetoIdToProjeto(Integer projetoId) {
@@ -37,12 +38,12 @@ public interface ChecklistMapper {
     }
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "nome", source = "nome")
-    @Mapping(target = "projeto", source = "projetoId", qualifiedByName = "projetoIdToProjeto")
-    @Mapping(target = "itens", ignore = true)
-    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "id",              ignore = true)
+    @Mapping(target = "nome",            source = "nome")
+    @Mapping(target = "projeto",         source = "projetoId", qualifiedByName = "projetoIdToProjeto")
+    @Mapping(target = "itens",           ignore = true)
+    @Mapping(target = "dataCriacao",     ignore = true)
     @Mapping(target = "dataAtualizacao", ignore = true)
-    @Mapping(target = "version", ignore = true)
-    void updateEntity(@MappingTarget Checklist entity, ChecklistRequestDTO dto);
+    @Mapping(target = "version",         ignore = true)
+    void updateFromDto(ChecklistUpdateDTO dto, @MappingTarget Checklist entity);
 }
